7fc162c812c33dced24d05c611127231ed6e8479
Nikita0503 <nikita0503ua@gmail.com>
2021-10-22 12:28:33 +0300

    login form markup

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+    loginForm: FormGroup;
 
-  ngOnInit(): void {
-  }
+    constructor(private fb: FormBuilder) {
+      this.loginForm = this.fb.group({
+        email: ['', Validators.email],
+        password: ['', Validators.minLength(6)]
+      });
+    }
 
+    login(){
+      console.log(this.loginForm.get('email')?.value)
+    }
+
+    get _invalidEmail() {
+      return this.loginForm.get('email')?.invalid
+    }
+
+    get _invalidPassword() {
+      return this.loginForm.get('password')?.invalid
+    }
+
+    get _disabledLogin() {
+      return this._invalidEmail || this._invalidPassword;
+    }
 }
